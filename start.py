caa4bf0f7f0c6471d2bee786661339ea729447e8
midsummerred32 <trevorleavitt08@gmail.com>
2021-06-17 18:38:03 -0400

    YAY

--- start.py
+++ start.py
@@ -1,34 +1,50 @@
 import os
+import trevor_directory
+
+#Asks if you want to install programs.
+def start():
+  doYouWantPrograms = input("Would you like to install programs? (y/n) ")
+  if doYouWantPrograms == "n" or "N":
+    startDirectory()
+  
+  elif doYouWantPrograms == "y" or "Y":
+    installing_programs()
+    
+  else:
+    print("Invalid Response")
+
 
 # Uses Winget to install programs.
 def installing_programs():
   print("Installing Programs!")
   os.system("winget import -i ./programs.json")
   print("Done!")
-  gathering_directory_info()
+  making_directory()
 
-# Creates my directory structrue.
-def gathering_directory_info():
-  print("Making Directory Structure!")
-  trevor_directory = input("Where would you like the Trevor directory? ")
-  print(trevor_directory)
-
-  def making_directory():
-    os.system(f"cd {trevor_directory}")
-    os.system("mkdir Trevor")
-    os.system("cd Trevor")
-    os.system("mkdir 3D Printer, Audiobooks, Coding, Documents, Downloads, Images Installer, MB Workbooks, Music, Other, ROMS, Unzipped ZIPS, Videos,VMs")
-    print("Done making directories.")
-
-  areYouSure = input("Are you sure this directory is correct? (y/n)")
-  if areYouSure == "n" or "N":
-    print("Run the script again with the correct path.")
-    exit()
+# Asks if you want Trevor Folder.
+def startDirectory():
+  doYouWantTrevorFolder = input("Would you like to make the Trevor Folder? (y/n) ")
   
-  if areYouSure != "y" or "Y" or "n" or "N":
+  if doYouWantTrevorFolder == "y" or "Y":
+    making_directory()
+  
+  elif doYouWantTrevorFolder == "n" or "N":
+    exit(code="Python Script is Done!")
+    
+  else:
     print("Invalid Response")
 
-  else:
-    making_directory()
+# Creates my directory structrue.
+def making_directory():
+  print("Making Directory Structure!")
+  print(trevor_directory.directory)
+  os.chdir(trevor_directory.directory)
+  os.system("mkdir Trevor")
+  os.chdir(".\Trevor")
+  os.system("mkdir 3D_Printer, Audiobooks, Coding, Documents, Downloads, Images, Installers, MB_Workbooks, Music, Other, ROMS, Unzipped_ZIPS, Videos, VMs")
+  print("Done making directories.")
+  exit(code="Python Script is Done!")
 
-installing_programs()
+
+
+start()
